c6d963d0d54ae649555e285c11bf3b06ea5f7de1
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-11-26 11:50:02 +0500

    Add files via upload

---
+++ Sem_1/1_1/1_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    if (a > 0 && b > 0) 
+    {
+        cout << a / b << endl;
+        return 0;
+    }
+    else if ((a == 0 || b == 0) && (a != 0 || b != 0)) 
+    {
+        cout << a / b << endl;
+        return 0;
+    }
+    else 
+    {
+        cout << "You can't divide by zero!" << endl;
+        return 0;
+    }
+}
