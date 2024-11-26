33787d29b89d6ee8747e9d34634dd479bed572ae
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-11-26 14:26:43 +0500

    Add files via upload

---
+++ Sem_1/1_3/1_3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  cin >> a;
+  if (a<5) 
+  {
+      cout << a*3 << endl;
+    }
+    else if (a>7) 
+    {
+        cout << a/10 << endl;
+    }
+    else 
+    {
+        cout << a+3 << endl;
+    }
+    return 0;
+}
