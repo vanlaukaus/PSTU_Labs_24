1c59b34f4311b0b76abf3abff214c2669c57f25f
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-25 00:07:26 +0500

    Update 2_5.cpp

--- Sem_1/2_5/2_5.cpp
+++ Sem_1/2_5/2_5.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	int n;
 	cin >> n;
-	if (n <= 2 ) { cout << "Erro! The number must be greater than 2" << endl; return 0; }
+	if (n <= 2 ) { cout << "Error! The number must be greater than 2" << endl; return 0; }
 	else 
 	{
 		for (int i = 0; i < n; i++)
