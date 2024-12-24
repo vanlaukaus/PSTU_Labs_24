5090511558c72551e2575a7048f17dc0bc171ce8
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-24 09:41:11 +0500

    Update 2_13.cpp

--- Sem_1/2_13/2_13.cpp
+++ Sem_1/2_13/2_13.cpp
@@ -5,9 +5,7 @@ int main()
 {
 	float a,b,c,d,x1,x2;
 	cin >> a >> b >> c;
-
 	d = pow(b,2) - (4 * a * c);
-
 	if (d > 0)
 		{
 			x1 = (-b + sqrt(d)) / (2*a);
@@ -20,6 +18,5 @@ int main()
 			cout << "x1 = x2 = " << x1 << endl;
 		}
 	else {cout << "No solutions";}
-
 	return 0;
 }
