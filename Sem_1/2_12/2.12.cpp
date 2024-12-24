86f13ec0e3448cbbd5004afa42474af72e35a103
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-24 09:32:45 +0500

    Add files via upload

---
+++ Sem_1/2_12/2.12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, t, max, min;
+	cin >> n;
+
+	cin >> t;
+	max = min = t;
+	for (int i=0; i<n-1; i++)
+	{
+		cin >> t;
+		if (t > max) {max = t;}
+		else if (t < min) {min = t;}
+	}
+	cout << max+min << endl;	
+
+	return 0;
+}
