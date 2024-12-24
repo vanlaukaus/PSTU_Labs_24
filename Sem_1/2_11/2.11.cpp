914aceaf96cec3c1929f085bdf4fb3c0b0709cfc
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-24 09:36:12 +0500

    Add files via upload

---
+++ Sem_1/2_11/2.11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,k;
+	cin >> n;
+	bool minus = false;
+	bool plus = false;
+
+	for(int i=1;i<=n;i++)
+	{
+		cin >> k;
+		if (!minus && !plus)
+		{
+			if(k>0){plus=true;}
+			else if(k<0){minus=true;}
+		}
+	}
+
+	if(plus){cout<<"Positive number";}
+	else if(minus){cout << "Negative number";}
+	else {cout << "Null";}
+
+	return 0;
+}
