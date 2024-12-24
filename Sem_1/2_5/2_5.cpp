96cf148e5ca5c47874d421c7d7bb50250c02d936
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-25 00:08:04 +0500

    Update 2_5.cpp

--- Sem_1/2_5/2_5.cpp
+++ Sem_1/2_5/2_5.cpp
@@ -4,12 +4,12 @@ int main()
 {
 	int n;
 	cin >> n;
-	if (n <= 2 ) { cout << "Error! The number must be greater than 2" << endl; return 0; }
+	if (n <= 2) {cout << "Error! The number must be greater than 2" << endl; return 0;}
 	else 
 	{
 		for (int i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n - i; j++) { cout << " "; }
+			for (int j = 0; j < n - i; j++) { cout << " ";}
 			for (int j = 0; j <= i; j++) { cout << "*"; }
 			cout << endl;
 		}
