7f77a32bf331f2fb93122eeb467af9536a9aa442
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-25 10:31:39 +0500

    Update 2.7.3.cpp

--- Sem_1/2_7/2.7.3.cpp
+++ Sem_1/2_7/2.7.3.cpp
@@ -1,25 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
+	for (int j = 0; j < n; j++) cout << "* ";
+	cout << endl;
+	for (int i = 0; i < n - 2; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
-			{
-				cout << "* ";
-			}
-			else
-			{
-				cout << "  ";
-			}
+		cout << "* ";
+		for (int j = 0; j < n - 2; j++) 
+		{ 
+			cout << "  "; 
 		}
-		cout << endl;
+		cout << "* " << endl;
 	}
+	for (int j = 0; j < n; j++) cout << "* ";
+	cout << endl;
 	return 0;
 }
-
 
