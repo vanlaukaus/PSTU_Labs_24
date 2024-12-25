6817e0da6c3c857d803bfacbeb7895f7b40f3b1d
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2024-12-25 10:23:42 +0500

    Update 2-16.cpp

--- Sem_1/2_16/2-16.cpp
+++ Sem_1/2_16/2-16.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
-int main () {
-    int N, S = 0, j = -1;
-    cout << "How many numbers should there be: ";
-    cin >> N;
-    for (int i = 1; i <= N; ++i) 
-    {
-        S += j * i;
-        j *= -1;
-    }
-    cout << "Sum: " << S << endl;
-    return 0;
+int main()
+{
+	int n, S=0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		{
+			if (i%2==0) {S += i;}
+			else {S -= i;}
+		}
+	cout << S << endl;
+	return 0;
 }
