45d736d3d6e67565d5d44bfccd7c845181ad5118
vanlaukaus <61706813+vanlaukaus@users.noreply.github.com>
2025-03-04 12:01:02 +0500

    Add files via upload

---
+++ Lab4.cpp
@@ -0,0 +1,32 @@
+//Вариант 15
+
+#include <iostream>
+using namespace std;
+int main() {
+    //1
+    int ring[10] = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+    int k;
+    cout << "1.1" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << ring[i] << " ";
+    }
+    cout << endl;
+    cout << "Enter k: ";
+    cin >> k;
+    //4
+    cout << "4.4" << endl;
+    for (int i = k; i < 10; i++)
+    {
+        cout << ring[i] << " ";
+        if (i == 9)
+        {
+            for (int ii = 0; ii <= k; ii++)
+            {
+                cout << ring[ii] << " ";
+            }
+        }
+    }
+    cout << endl;
+    return 0;
+}
